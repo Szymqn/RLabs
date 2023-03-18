0616ef44193afd7bf018d0486d03ab864e555798
szymqn <koodziejskisz@outlook.com>
2023-03-18 01:08:52 +0100

    create pie plot

--- lab2/main.R
+++ lab2/main.R
@@ -28,6 +28,20 @@ p1
 
 p1 + theme_linedraw()
 
-p2 <- ggplot(Lang)
+summary(acsNew)
+
+p2 <- acsNew %>%
+  group_by(Language) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count)) %>%
+  mutate(prop = count / sum(count) * 100) %>%
+  mutate(ypos = cumsum(prop) - 0.5 * prop) %>%
+  ggplot(aes(x = "", y = prop, fill = `Language`)) +
+  geom_bar(stat = "identity", width = 1, color = "white") + 
+  coord_polar("y", start = 0) +
+  theme_void() +
+  theme(plot.title = element_text(hjust = 0.5, color = 'peru'),
+        plot.subtitle = element_text(hjust = 1, color = 'yellowgreen'))
 
 p2
+
