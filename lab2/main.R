9514e73b9bd48007b4dbf11653ca81d6fe1c4eb1
szymqn <koodziejskisz@outlook.com>
2023-03-18 01:18:27 +0100

    create circle chart

--- lab2/main.R
+++ lab2/main.R
@@ -1,7 +1,3 @@
-# Zbuduj odpowiadający mu wykres kołowy.
-# Zbuduj odpowiadający mu wykres pierścieniowy.
-
-
 library(tidyverse)
 
 acsNew <- read_csv("http://www.jaredlander.com/data/acsNew.csv")
@@ -41,7 +37,25 @@ p2 <- acsNew %>%
   coord_polar("y", start = 0) +
   theme_void() +
   theme(plot.title = element_text(hjust = 0.5, color = 'peru'),
-        plot.subtitle = element_text(hjust = 1, color = 'yellowgreen'))
+        plot.subtitle = element_text(hjust = 1, color = 'yellowgreen')) +
+  scale_fill_discrete(name = "Języki", labels = l1)
 
 p2
+
+p3 <- acsNew %>%
+  group_by(Language) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count)) %>%
+  mutate(fraction = count / sum(count),
+         ymax = cumsum(fraction),
+         ymin = c(0, head(ymax, n = -1))) %>%
+  ggplot(aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = Language)) +
+  geom_rect() +
+  coord_polar(theta = "y") +
+  xlim(c(2, 4)) +
+  theme_void() +
+  scale_fill_discrete(name = "Języki", labels = l1)
+
+p3
+
 
